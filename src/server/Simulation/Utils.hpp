fedd8faa17d482bc06431590f82c4f8ec2acb8c7
theo-futol <theo-futol@epitech.eu>
2026-06-17 19:12:05 +0400

    style: use enum for corner

--- src/server/Simulation/Utils.hpp
+++ src/server/Simulation/Utils.hpp
@@ -2,6 +2,18 @@
 
 namespace zappy
 {
+
+enum Corner
+{
+    NORTH = 0,
+    NORTH_EAST = 45,
+    EAST = 90,
+    EAST_SOUTH = 135,
+    SOUTH = 180,
+    SOUTH_WEST = 225,
+    WEST = 270
+};
+
 enum class PlayerState
 {
     PENDING,
