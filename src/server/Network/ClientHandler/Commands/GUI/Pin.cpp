52437bef4ca6ab405baf066982131d19b240419f
Hawk-Eye <arthur.girardin-calbe@epitech.eu>
2026-06-18 15:53:13 +0400

    feat: ppo

---
+++ src/server/Network/ClientHandler/Commands/GUI/Pin.cpp
@@ -0,0 +1,11 @@
+#include "../Commands.hpp"
+
+namespace zappy
+{
+std::string Commands::Pin(std::vector<std::string> args, Client &client)
+{
+    (void)client; // Unused parameter
+    (void)args;   // Unused parameter
+    return "";
+}
+}
