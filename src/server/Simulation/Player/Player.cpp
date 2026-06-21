68f907284986607c2142245d862fc88d4482a797
theo-futol <theo-futol@epitech.eu>
2026-06-21 18:50:41 +0400

    fix(server): correct toroidal distance calculation in getDirectionTo

--- src/server/Simulation/Player/Player.cpp
+++ src/server/Simulation/Player/Player.cpp
@@ -149,10 +149,10 @@ Degrees Player::getDirectionTo(const position &target, std::pair<int, int> mapSi
     int dx = target.x - _pos.x;
     int dy = target.y - _pos.y;
     if (std::abs(dx) > (mapSize.first - std::abs(dx)))
-        dx = mapSize.first - dx;
+        dx = mapSize.first - std::abs(dx);
     if (std::abs(dy) > (mapSize.second - std::abs(dy)))
-        dy = mapSize.second - dy;
-    return Direction::getNearestDirection(std::atan2(dy, dx) * 100);
+        dy = mapSize.second - std::abs(dy);
+    return Direction::getNearestDirection(std::atan2(dy, dx) * 180 / M_PI);
 }
 
 void Player::setState(PlayerState newState)
