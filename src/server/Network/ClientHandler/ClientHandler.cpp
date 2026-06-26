412e02f98eaae8701de817f87d04fa329ea516f8
theo-futol <theo-futol@epitech.eu>
2026-06-26 14:54:41 +0400

    fix: add pretty message for end

--- src/server/Network/ClientHandler/ClientHandler.cpp
+++ src/server/Network/ClientHandler/ClientHandler.cpp
@@ -78,6 +78,11 @@ void ClientHandler::handleClients(void)
             *_serverIsRunning = false;
         broadcastGuiInfo();
         broadcastMessageToClients();
+        Team *winningTeam = _world->getWinningTeam();
+        if (winningTeam)
+            for (const auto &player : _world->getPlayers())
+                if (player->getTeam()._name == winningTeam->_name)
+                    std::cout << "Player " << player->getId() << " was level: " << player->getLevel() << std::endl;
     }
 }
 
